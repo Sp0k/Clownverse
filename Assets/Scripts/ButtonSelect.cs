87ccf3fad160dc397c4d7c98b5125edef49713df
Anthony <anthonydaniel123@live.com>
2024-01-27 19:51:24 -0400

    Kinda bug fix not really

--- Assets/Scripts/ButtonSelect.cs
+++ Assets/Scripts/ButtonSelect.cs
@@ -7,25 +7,26 @@ using UnityEngine.UI;
 public class ButtonSelect : MonoBehaviour
 {
     public Slider slider;
+    public Image image;
     public GameObject firstButton;
     [Range(0.01f, 1)]
     public float speed;
 
-    Image image;
+    GameObject lastSelectedButton;
+
 
     private void Start()
     {
         slider.interactable = false;
-        image = GetComponent<Image>();
         EventSystem.current.SetSelectedGameObject(firstButton, new BaseEventData(EventSystem.current));
+        lastSelectedButton = firstButton;
     }
 
     void Update()
     {
         if (EventSystem.current.currentSelectedGameObject == null || EventSystem.current.currentSelectedGameObject.GetComponent<Button>() == null) 
         {
-            image.enabled = false;
-            return;
+            EventSystem.current.SetSelectedGameObject(lastSelectedButton, new BaseEventData(EventSystem.current));
         }
 
         Vector3 buttonPos = EventSystem.current.currentSelectedGameObject.transform.position;
