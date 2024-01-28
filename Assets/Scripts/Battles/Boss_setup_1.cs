e34e1f6ce42451ae290c4038b999cf5a7150015d
Sp0k <gabrielsavard.prog@gmail.com>
2024-01-27 22:58:58 -0400

    Added first boss setup

--- Assets/Scripts/Battles/Boss_setup_1.cs
+++ Assets/Scripts/Battles/Boss_setup_1.cs
@@ -9,27 +9,51 @@ public class Boss_setup_1 : MonoBehaviour
     private int bad = 20;
     private int mediocre = 50;
 
+    private Joke[] jokes = new Joke[5];
+
     // Start is called before the first frame update
     void Start()
     {
-        Joke[] jokes = new Joke[5];
-        for (int i = 0; i < jokes.Length; i++)
-        {
-            jokes[i] = new Joke();
-        }
-        
-        // First Joke
-        // Punchlines
-        Punchline[] punchlines = new Punchline[3];
-        for (int i = 0; i < punchlines.Length; i++)
-        {
-            punchlines[i] = new Punchline();
-        }
-        punchlines[0].setValues("bad", bad, false);
-        punchlines[1].setValues("good", good, true);
-        punchlines[2].setValues("mediocre", mediocre, false);
+        Punchline[] punchlines;
 
-        // Joke
-        .setValues("Joke", punchlines);
+        // First Joke
+        punchlines = new Punchline[3];
+        punchlines[0] = new Punchline("bad 1", bad, false);
+        punchlines[1]= new Punchline ("good 1", good, true);
+        punchlines[2]= new Punchline("mediocre 1", mediocre, false);
+
+        jokes[0] = new Joke(punchlines, "Joke 1");
+
+        // Second Joke
+        punchlines = new Punchline[3];
+        punchlines[0] = new Punchline("mediocre 2", mediocre, false);
+        punchlines[1] = new Punchline("bad 2", bad, false);
+        punchlines[2] = new Punchline("good 2", good, true);
+
+        jokes[1] = new Joke(punchlines, "Joke 2");
+
+        // Third Joke
+        punchlines = new Punchline[3];
+        punchlines[0] = new Punchline("good 3", good, true);
+        punchlines[1] = new Punchline("mediocre 3", mediocre, false);
+        punchlines[2] = new Punchline("bad 3", bad, false);
+
+        jokes[2] = new Joke(punchlines, "Joke 3");
+
+        // Fourth Joke
+        punchlines = new Punchline[3];
+        punchlines[0] = new Punchline("bad 4", bad, false);
+        punchlines[1] = new Punchline("mediocre 4", mediocre, false);
+        punchlines[2] = new Punchline("good ", good, true);
+
+        jokes[3] = new Joke(punchlines, "Joke 4");
+
+        // Fifth Joke
+        punchlines = new Punchline[3];
+        punchlines[0] = new Punchline("good 5", good, true);
+        punchlines[1] = new Punchline("bad 5", bad, false);
+        punchlines[1] = new Punchline("mediocre 5", mediocre, false);
     }
+
+    public Joke[] getJokes() { return jokes; }
 }
