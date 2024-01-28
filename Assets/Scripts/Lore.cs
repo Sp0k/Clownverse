d432c305c8ad516fb01cbd454f29c20cfbac28a0
Charlie Baker <charliebaker@bakeryproductions.com>
2024-01-28 16:38:02 -0400

    PLEASE

--- Assets/Scripts/Lore.cs
+++ Assets/Scripts/Lore.cs
@@ -11,9 +11,9 @@ public class Lore : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        text.Add("Welcome to Clown College, You have worked tirelessy to get here and are ready to begin your journey to become the Funniest Clown.");
-        text.Add("But when you seem arrive things seem... off. An oddly unfunny haze has covered the campus, there is a dark hue to it.");
-        text.Add("It seems that the proffessors of the prestigeous Clown College have been corrupted by an entity known as the Void Clown.");
+        text.Add("Welcome to Clown College, You have worked tirelessly to get here and are ready to begin your journey to become the Funniest Clown.");
+        text.Add("But when you arrive things seem... off. An oddly unfunny haze has covered the campus, there is a dark hue to it.");
+        text.Add("It seems that the professors of the prestigious Clown College have been corrupted by an entity known as the Void Clown.");
         text.Add("Their laugh-force is being siphoned to the anti-clown world. You remain uncorrupted and you need to purify them and restore their laughter.");
         text.Add("It is up to you to save clown society.");
         textBox.text = text[textNumber];
